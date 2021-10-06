be2538fd3809706ecac05fbbe51718f5e7d282cf
Mike Rousos <Mike@MJRousos.com>
2021-10-06 10:50:25 -0400

    Add web API with B2C auth

---
+++ GroupManagerAPI/Controllers/CodeNameController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web.Resource;
+using System;
+using System.Linq;
+
+namespace GroupManagerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CodeNameController : ControllerBase
+    {
+        private readonly ILogger<CodeNameController> _logger;
+
+
+        public CodeNameController(ILogger<CodeNameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+        [RequiredScope("read")]
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var userName = User.Identity.Name;
+
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                return Unauthorized();
+            }
+
+            return Ok(new string(userName.Reverse().ToArray()));
+        }
+    }
+}

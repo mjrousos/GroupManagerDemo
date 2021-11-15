6aeb716dd3243920d4fb2bdba5eb46532d4b33ed
Mike Rousos <Mike@MJRousos.com>
2021-11-15 17:14:31 -0500

    Retrieve secrets from Key Vault

---
+++ GroupManager/GroupManagerKeyVaultSecretManager.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+
+namespace GroupManager
+{
+    public class GroupManagerKeyVaultSecretManager : KeyVaultSecretManager
+    {
+        public override string GetKey(KeyVaultSecret secret) => secret.Name.Replace("--", ConfigurationPath.KeyDelimiter);
+    }
+}
--- GroupManager/Program.cs
+++ GroupManager/Program.cs
@@ -1,5 +1,10 @@
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace B2CDemo
 {
@@ -14,6 +19,15 @@ namespace B2CDemo
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureAppConfiguration((context, config) =>
+                    {
+                        var builtConfig = config.Build();
+                        if (context.HostingEnvironment.IsProduction())
+                        {
+                            var secretClient = new SecretClient(new Uri(builtConfig["KeyVaultUri"]), new DefaultAzureCredential());
+                            config.AddAzureKeyVault(secretClient, new AzureKeyVaultConfigurationOptions());
+                        }
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }
